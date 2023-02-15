4af80146fcd5cfd2c1f023482fb7145f93f96852
Sylvain Munaut <tnt@246tNt.com>
2023-02-15 12:13:25 +0100

    Add OVERCLOCK option
    This adds the option to run things at 20 MHz instead of 15 MHz which
    is about 50% faster than the fmax reported by nextpnr, but in good
    condition (good/high vcore, normal temp, ...) works OK.
    
    UART divider need to be updated at various places though, so adapt
    for this too
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- gateware/riscv_linux/data/clocks.py
+++ gateware/riscv_linux/data/clocks.py
@@ -1,4 +1,10 @@
-BASE = 15
+# Find PLL
+pll = [v for k,v in ctx.cells if 'PLL' in v.type][0]
+
+BASE = {
+	('0000', '1001111', '100'): 15,
+	('0000', '0110100', '011'): 20,
+}[(pll.params['DIVR'], pll.params['DIVF'], pll.params['DIVQ'])]
 
 ctx.addClock("clk_1x", 1 * BASE)
 ctx.addClock("clk_4x", 4 * BASE)
